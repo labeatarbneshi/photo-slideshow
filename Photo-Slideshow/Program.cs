ae5df7f27f59b26effa89b5e6d018d90e6de4230
Labeat Arbneshi <labeat.arbneshi@gmail.com>
2020-01-30 00:05:12 +0100

    added function to read files

--- Photo-Slideshow/Program.cs
+++ Photo-Slideshow/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PhotoSlideshow.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,46 @@ namespace PhotoSlideshow
     {
         static void Main(string[] args)
         {
+            ReadFile();
+
+            Console.ReadKey();
+        }
+
+        static void ReadFile()
+        {
+            Collection collection = new Collection();
+            var fileStream = new FileStream(@"C:\dev\photo-slideshow\Photo-Slideshow\Instances\c_memorable_moments.txt", FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                int lineNumber = 1;
+                collection.Photos = new List<Photo>();
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    if(lineNumber == 1)
+                    {
+                        collection.Size = int.Parse(line);
+                    }
+                    else
+                    {
+                        collection.Photos.Add(ProcessLine(line, lineNumber));
+                    }
+                    lineNumber++;
+                }
+            }
+        }
+
+        static Photo ProcessLine(string line, int lineNo)
+        {
+            string[] lineElements = line.Split(' ');
+            Photo photo = new Photo
+            {
+                Id = lineNo,
+                Orientation = lineElements[0] == "H" ? Enums.Orientation.HORIZONTAL : Enums.Orientation.VERTICAL,
+                NumberOfTags = int.Parse(lineElements[1]),
+                Tags = lineElements.Skip(2).Take(lineElements.Length - 1).ToList()
+            };
+            return photo;
         }
     }
 }
