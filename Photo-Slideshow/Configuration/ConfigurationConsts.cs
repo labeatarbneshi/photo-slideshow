2301d92f51682c18549440ef564ac9dce92a3a42
Labeat Arbneshi <labeat.arbneshi@gmail.com>
2020-11-04 21:08:20 +0100

    changed config consts

--- Photo-Slideshow/Configuration/ConfigurationConsts.cs
+++ Photo-Slideshow/Configuration/ConfigurationConsts.cs
@@ -8,14 +8,21 @@ namespace PhotoSlideshow.Configuration
 {
     public class ConfigurationConsts
     {
+        // Total algorithm run duration
         public static long RunDuration = 1800000;
-        public static int SlideSwapUpperFrequency = 4;
-        public static int VerticalPhotoSwapFrequencyLowerLimit = SlideSwapUpperFrequency;
-        public static int VerticalPhotoSwapFrequencyUpperLimit = 7;
-        public static int ShuffleOperatorFrequency = VerticalPhotoSwapFrequencyUpperLimit;
-        public static int AcceptBadSolutionAfterMillis = 5000;
-        public static int RetriesAfterBadVerticalSwap = 15;
-        public static bool SkipVerticalSwap = false;
+
+        // From 1 - 4 -> Slide swap operator
+        public static int SlideSwapUpperFrequency = 3;
+
+        // From this to VerticalPhotoSwapFrequencyUpperLimit -> Photo swap operator
+        public static int VerticalPhotoSwapFrequencyLowerLimit = 3;
+        public static int VerticalPhotoSwapFrequencyUpperLimit = 9;
+
+        // Shuffle swap operator frequency
+        public static int ShuffleOperatorFrequency = 9;
+
+        public static int AcceptWorseSolutionAfterMillis = 5000;
+        public static int RetriesAfterBadVerticalSwap = 110;
         public static int AcceptWorseSolutionAfterNoProgressMillis = 0;
     }
 }
