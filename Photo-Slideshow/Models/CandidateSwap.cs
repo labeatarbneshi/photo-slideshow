82f78dc9f1c962874dd3ee43d56d97291b636665
Labeat Arbneshi <labeat.arbneshi@gmail.com>
2020-11-04 21:10:55 +0100

    removed unused models

--- Photo-Slideshow/Models/CandidateSwap.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PhotoSlideshow.Models
-{
-    public class CandidateSwap
-    {
-        public int SlideId { get; set; }
-        public int Index { get; set; }
-        public int Score { get; set; }
-    }
-}
